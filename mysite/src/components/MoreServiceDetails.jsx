5b605bc5e88bdb1a6ab07d28424c535b7ad4b7b1
Ikendu <ndubest56@gmail.com>
2024-11-20 06:19:57 +0100

    our service

---
+++ mysite/src/components/MoreServiceDetails.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function MoreServiceDetails() {
+  return (
+    <div>
+      <p>
+        💐 Floral Masterpieces: Stunning floral arrangements that set the tone
+        for elegance and beauty.
+      </p>
+      <p>
+        🎀 Customized Themes: Whether it’s vintage chic, modern luxe, or
+        whimsical fun, we bring your dream theme to life.
+      </p>
+      <p>
+        💡 Lighting & Ambience: The perfect glow, the perfect mood—our lighting
+        solutions take your décor to the next level.
+      </p>
+      <p>
+        📸 Picture-Perfect Backdrops: Instagram-worthy moments? Consider it
+        done!
+      </p>
+    </div>
+  );
+}
--- mysite/src/components/OurService.jsx
+++ mysite/src/components/OurService.jsx
@@ -2,17 +2,29 @@ import React from "react";
 
 export default function OurService() {
   return (
-    <div>
-      <div className="service">
-        <h2 className="serviceHeader">Our Services</h2>
-        <p className="servicePara">
-          ✨ Weddings & Engagements: Romantic, breathtaking designs that make
-          your big day unforgettable. 🎉 Corporate Events: Professional,
-          polished setups for launches, awards nights, and conferences. 🎈
-          Private Parties: Birthdays, anniversaries, and everything in
-          between—celebrate in style. 🌺 Themed Events: From boho-chic to
-          classic glam, we bring your vision to life.
-        </p>
+    <div className="service">
+      <div className="overlay2">
+        <div className="serviceCover">
+          <h2 className="serviceHeader">Our Services</h2>
+          <div className="servicePara">
+            <p>
+              ✨ Weddings & Engagements: Romantic, breathtaking designs that
+              make your big day unforgettable.
+            </p>
+            <p>
+              🎉 Corporate Events: Professional, polished setups for launches,
+              awards nights, and conferences.
+            </p>
+            <p>
+              🌺 Themed Events: From boho-chic to classic glam, we bring your
+              vision to life.
+            </p>
+            <p>
+              🎈 Private Parties: Birthdays, anniversaries, and everything in
+              between—celebrate in style.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
