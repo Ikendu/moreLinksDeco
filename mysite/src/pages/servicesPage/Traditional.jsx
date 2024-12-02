3004bc499e1561bf14ff3dc8b98eb66fdd641d7b
Ikendu <ndubest56@gmail.com>
2024-12-02 06:21:33 +0100

    more on events

--- mysite/src/pages/servicesPage/Traditional.jsx
+++ mysite/src/pages/servicesPage/Traditional.jsx
@@ -2,7 +2,8 @@ import Getter from "../../assets/images/getter";
 import image10 from "../../assets/images/image10.jpg";
 import image15 from "../../assets/images/image15.jpg";
 import image16 from "../../assets/images/image16.jpg";
-import image17 from "../../assets/images/image18.jpg";
+import image18 from "../../assets/images/image18.jpg";
+import image19 from "../../assets/images/image19.jpg";
 Getter;
 export default function Traditional() {
   return (
@@ -17,8 +18,11 @@ export default function Traditional() {
         <a href={image16}>
           <img src={image16} alt="" />
         </a>
-        <a href={image17}>
-          <img src={image17} alt="" />
+        <a href={image18}>
+          <img src={image18} alt="" />
+        </a>
+        <a href={image19}>
+          <img src={image19} alt="" />
         </a>
       </div>
     </div>
