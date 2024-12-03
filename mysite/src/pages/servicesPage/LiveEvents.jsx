40e228769132fd71bce2ece78b11e44146e314f6
Ikendu <ndubest56@gmail.com>
2024-12-03 04:45:11 +0100

    lives event

---
+++ mysite/src/pages/servicesPage/LiveEvents.jsx
@@ -0,0 +1,42 @@
+import Getter from "../../assets/images/getter";
+import sixth from "../../assets/images/sixth.jpg";
+import image13 from "../../assets/images/image27.jpg";
+import image12 from "../../assets/images/image28.jpg";
+import image20 from "../../assets/images/image29.jpg";
+import image22 from "../../assets/images/image30.jpg";
+import image26 from "../../assets/images/image31.jpg";
+import image24 from "../../assets/images/image32.jpg";
+// import image4 from "../../assets/images/image33.jpeg";
+Getter;
+export default function LiveEvent() {
+  return (
+    <div>
+      <div className="genImage">
+        <a href={sixth}>
+          <img src={sixth} alt="" />
+        </a>
+        <a href={image13}>
+          <img src={image13} alt="" />
+        </a>
+        <a href={image12}>
+          <img src={image12} alt="" />
+        </a>
+        <a href={image20}>
+          <img src={image20} alt="" />
+        </a>
+        <a href={image22}>
+          <img src={image22} alt="" />
+        </a>
+        <a href={image26}>
+          <img src={image26} alt="" />
+        </a>
+        <a href={image24}>
+          <img src={image24} alt="" />
+        </a>
+        <a href={image4}>
+          <img src={image4} alt="" />
+        </a>
+      </div>
+    </div>
+  );
+}
