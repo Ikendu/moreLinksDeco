6fe3dd60bd48d1cc17886ad57156e002318b1810
Ikendu <ndubest56@gmail.com>
2024-12-04 06:57:35 +0100

    empty component handuler

--- mysite/src/components/EmptyComp/EmptyComp.jsx
+++ mysite/src/components/EmptyComp/EmptyComp.jsx
@@ -1,10 +1,12 @@
 import logoImage from "../../assets/images/logoImage.png";
 import image1 from "../../assets/images/image1.jpeg";
 import image2 from "../../assets/images/image2.jpeg";
+import "./empty.css";
 export default function () {
   return (
-    <div>
-      <h4>Pictures Loading....</h4>
+    <div className="empty">
+      <h2>Items not yet ready</h2>
+      <h4>Check back later</h4>
       <div className="emptyComp">
         <a href={logoImage}>
           <img src={logoImage} alt="" />
