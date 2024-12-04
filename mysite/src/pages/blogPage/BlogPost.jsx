f1ef7e12ab72813188cace24961b4ba0136c7e0a
Ikendu <ndubest56@gmail.com>
2024-12-04 07:29:50 +0100

    whatsapp component handuler

--- mysite/src/pages/blogPage/BlogPost.jsx
+++ mysite/src/pages/blogPage/BlogPost.jsx
@@ -1,10 +1,13 @@
+import EmptyComp from "../../components/EmptyComp/EmptyComp";
 import ScrollToTop from "../../components/ScrollToTop";
 import "./blog.css";
 export default function BlogPost() {
   return (
     <div>
       <ScrollToTop />
-      <div></div>
+      <div className="blog">
+        <EmptyComp />
+      </div>
     </div>
   );
 }
--- mysite/src/pages/gallaryPage/Gallary.jsx
+++ mysite/src/pages/gallaryPage/Gallary.jsx
@@ -1,5 +1,10 @@
+import EmptyComp from "../../components/EmptyComp/EmptyComp";
 import "./gallary.css";
 
 export default function Gallary() {
-  return <div>Gallary</div>;
+  return (
+    <div className="gallary">
+      <EmptyComp />
+    </div>
+  );
 }
