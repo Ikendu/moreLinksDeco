03093620b2d6a0e269d95b30d18ad3457900f291
Ikendu <ndubest56@gmail.com>
2024-12-05 14:11:32 +0100

    axos and backend links

--- mysite/src/components/FrontDetailsImage.jsx
+++ mysite/src/components/FrontDetailsImage.jsx
@@ -6,10 +6,10 @@ export default function FrontDetailsImage({ header, para, reverse, img }) {
       <div className="pageContent">
         <h3>🎨 Custom Creations, Perfectly Your likeness</h3>
         <p>
-          No two events are alike,jhgjkgkfgdf and neither are our designs.
-          Whether you dream of a chic minimalist wedding or a vibrant corporate
-          gala, our team works closely with you to craft unique designs that
-          speak to your personality and purpose .
+          No two events are alike, and neither are our designs. Whether you
+          dream of a chic minimalist wedding or a vibrant corporate gala, our
+          team works closely with you to craft unique designs that speak to your
+          personality and purpose .
         </p>
       </div>
       <img src={image1} alt="" className="serviceIage" />
