c68751a24cae6001947c7dd03265b70d065e48fc
Aniede Chibundu David <ndubest56@gmail.com>
2024-12-05 14:08:13 +0100

    Update FrontDetailsImage.jsx

--- mysite/src/components/FrontDetailsImage.jsx
+++ mysite/src/components/FrontDetailsImage.jsx
@@ -9,7 +9,7 @@ export default function FrontDetailsImage({ header, para, reverse, img }) {
           No two events are alike, and neither are our designs. Whether you
           dream of a chic minimalist wedding or a vibrant corporate gala, our
           team works closely with you to craft unique designs that speak to your
-          personality and purpose.
+          personality and purpose fgdfjhhjhgd.
         </p>
       </div>
       <img src={image1} alt="" className="serviceIage" />
