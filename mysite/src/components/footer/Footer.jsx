f4166028c510777325f72483b0b2ac0088cd2667
Ikendu <ndubest56@gmail.com>
2024-12-05 08:35:53 +0100

    contact functions

--- mysite/src/components/footer/Footer.jsx
+++ mysite/src/components/footer/Footer.jsx
@@ -81,20 +81,26 @@ export default function Footer() {
               <h2>Let get social</h2>
               <ul className="socialLinks">
                 <li>
-                  <a href="">
+                  <a
+                    href="https://web.facebook.com/profile.php?id=61569179348562"
+                    target="_blank"
+                  >
                     <i class="fa-brands fa-facebook-f"></i>
                   </a>
                 </li>
                 <li>
-                  <a href="">
+                  <a
+                    href="https://www.instagram.com/morelink.concept/"
+                    target="_blank"
+                  >
                     <i class="fa-brands fa-instagram"></i>
                   </a>
                 </li>
-                <li>
-                  <a href="">
+                {/* <li>
+                  <a href="" target="_blank">
                     <i class="fa-brands fa-twitter"></i>
                   </a>
-                </li>
+                </li> */}
                 <li>
                   <a
                     href="https://wa.me/2348061632276?text=Hello%20there!%20I%20want%20to%20discuss%20with%20you."
--- mysite/src/pages/servicesPage/Engagement.jsx
+++ mysite/src/pages/servicesPage/Engagement.jsx
@@ -2,7 +2,8 @@ import Getter from "../../assets/images/getter";
 import image14 from "../../assets/images/image14.jpg";
 import image9 from "../../assets/images/image9.jpg";
 import image8 from "../../assets/images/image8.jpg";
-import image17 from "../../assets/images/image18.jpg";
+import image90 from "../../assets/images/image9.0.jpg";
+import image91 from "../../assets/images/image9.1.jpg";
 Getter;
 export default function Engagement() {
   return (
@@ -17,8 +18,11 @@ export default function Engagement() {
         <a href={image8}>
           <img src={image8} alt="" />
         </a>
-        <a href={image14}>
-          <img src={image14} alt="" />
+        <a href={image90}>
+          <img src={image90} alt="" />
+        </a>
+        <a href={image91}>
+          <img src={image91} alt="" />
         </a>
       </div>
     </div>
