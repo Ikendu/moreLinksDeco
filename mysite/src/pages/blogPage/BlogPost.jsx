c43bd72a8b4e23cdd8d5ac565cd94fd98a889c23
Ikendu <ndubest56@gmail.com>
2024-12-08 13:05:20 +0100

    blog page

--- mysite/src/pages/blogPage/BlogPost.jsx
+++ mysite/src/pages/blogPage/BlogPost.jsx
@@ -2,8 +2,31 @@ import { useNavigate } from "react-router-dom";
 import EmptyComp from "../../components/EmptyComp/EmptyComp";
 import ScrollToTop from "../../components/ScrollToTop";
 import "./blog.css";
-export default function BlogPost() {
+import CommentSection from "./CommentSection";
+
+export default function BlogPost({ postId }) {
   const navigate = useNavigate();
+
+  const [post, setPost] = useState({});
+  const [likeCount, setLikeCount] = useState(0);
+  const [loveCount, setLoveCount] = useState(0);
+
+  useEffect(() => {
+    // Fetch post details
+    axios.get(`/api/post/${postId}`).then((response) => {
+      setPost(response.data.post);
+      setLikeCount(response.data.likes.like);
+      setLoveCount(response.data.likes.love);
+    });
+  }, [postId]);
+
+  const handleReaction = (type) => {
+    axios.post(`/api/post/${postId}/react`, { type }).then((response) => {
+      if (type === "like") setLikeCount((prev) => prev + 1);
+      else setLoveCount((prev) => prev + 1);
+    });
+  };
+
   return (
     <div>
       <ScrollToTop />
