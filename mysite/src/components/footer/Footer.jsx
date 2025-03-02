dc33840701d3292cc83d7e8bc4dc63b5bdb289eb
Chibundu <ndubest56@gmail.com>
2025-03-02 13:33:13 +0100

    more on whatsapp button

--- mysite/src/components/footer/Footer.jsx
+++ mysite/src/components/footer/Footer.jsx
@@ -1,99 +1,98 @@
-import React from "react";
-import "./footer.css";
+import React from 'react'
+import './footer.css'
 
-import logo from "../../assets/images/logoImage.png";
-import Getter from "../../assets/images/getter";
-import { Link } from "react-router-dom";
+import logo from '../../assets/images/logoImage.png'
+import Getter from '../../assets/images/getter'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
   return (
-    <div className="footerContainer">
-      <div className="footer">
-        <div className="imageContainer">
-          <img src={logo} alt="" className="imageFooter" />
+    <div className='footerContainer'>
+      <div className='footer'>
+        <div className='imageContainer'>
+          <img src={logo} alt='' className='imageFooter' />
         </div>
-        <div className="footerContents">
-          <div className="centerControl">
+        <div className='footerContents'>
+          <div className='centerControl'>
             <div>
               <h2>Let’s Stay Connected</h2>
-              <div className="contact">
-                <i class="fa-solid fa-phone"></i>
-                <p>+234 806 163 2276</p>
+              <div className='contact'>
+                <i className='fa-solid fa-phone'></i>
+                <p>0807 095 8827</p>
               </div>
-              <div className="contact">
-                <i class="fa-solid fa-envelope"></i>
+              <div className='contact'>
+                <i className='fa-solid fa-phone'></i>
+                <p>0806 163 2276</p>
+              </div>
+
+              <div className='contact'>
+                <i className='fa-solid fa-envelope'></i>
                 <p>service@moredeco.com.ng</p>
               </div>
-              <div className="contact">
-                <i class="fa-solid fa-location-dot"></i>
+              <div className='contact'>
+                <i className='fa-solid fa-location-dot'></i>
                 <p>30 Adelabor Street Uwani Enugu</p>
               </div>
             </div>
             <div>
               <h2>✨&nbsp;Services&nbsp;&&nbsp;Solutions</h2>
-              <ul className="serviceList">
+              <ul className='serviceList'>
                 <li>
-                  <Link to="/gallery">🎈 Event Styling</Link>
+                  <Link to='/gallery'>🎈 Event Styling</Link>
                 </li>
                 <li>
-                  <a href="/gallery">💐 Floral Arrangements</a>
+                  <a href='/gallery'>💐 Floral Arrangements</a>
                 </li>
                 <li>
-                  <Link to="/gallery">🎀 Custom Decor </Link>
+                  <Link to='/gallery'>🎀 Custom Decor </Link>
                 </li>
                 <li>
-                  <Link to="/gallery">🚨 Lighting & Ambience</Link>
+                  <Link to='/gallery'>🚨 Lighting & Ambience</Link>
                 </li>
                 <li>
-                  <Link to="/gallery">📸 Instagram-Worthy Setups</Link>
+                  <Link to='/gallery'>📸 Instagram-Worthy Setups</Link>
                 </li>
                 <li>
-                  <Link to="/gallery">🌺 Explore Our Gallary 🌺</Link>
+                  <Link to='/gallery'>🌺 Explore Our Gallary 🌺</Link>
                 </li>
               </ul>
             </div>
           </div>
-          <div className="centerControl">
-            <div className="quickLinks">
+          <div className='centerControl'>
+            <div className='quickLinks'>
               <h2>Quick Links</h2>
-              <ul className="footerLinks">
+              <ul className='footerLinks'>
                 <li>
-                  <Link to="/about">About Us</Link>
+                  <Link to='/about'>About Us</Link>
                 </li>
                 <li>
-                  <Link to="/services">Our Solutions</Link>
+                  <Link to='/services'>Our Solutions</Link>
                 </li>
                 <li>
-                  <Link to="/contact">Contact us</Link>
+                  <Link to='/contact'>Contact us</Link>
                 </li>
                 {/* <li>
                   <Link to="/testimony">Clients Testimonies</Link>
                 </li> */}
                 <li>
-                  <Link to="/blogpost">Event planning trends</Link>
+                  <Link to='/blogpost'>Event planning trends</Link>
                 </li>
                 <li>
-                  <Link to="/gallery">Our Gallary</Link>
+                  <Link to='/gallery'>Our Gallary</Link>
                 </li>
               </ul>
             </div>
             <div>
               <h2>Let get social</h2>
-              <ul className="socialLinks">
+              <ul className='socialLinks'>
                 <li>
-                  <a
-                    href="https://web.facebook.com/profile.php?id=61569179348562"
-                    target="_blank"
-                  >
-                    <i class="fa-brands fa-facebook-f"></i>
+                  <a href='https://web.facebook.com/profile.php?id=61569179348562' target='_blank'>
+                    <i className='fa-brands fa-facebook-f'></i>
                   </a>
                 </li>
                 <li>
-                  <a
-                    href="https://www.instagram.com/morelink.concept/"
-                    target="_blank"
-                  >
-                    <i class="fa-brands fa-instagram"></i>
+                  <a href='https://www.instagram.com/morelink.concept/' target='_blank'>
+                    <i className='fa-brands fa-instagram'></i>
                   </a>
                 </li>
                 {/* <li>
@@ -103,25 +102,25 @@ export default function Footer() {
                 </li> */}
                 <li>
                   <a
-                    href="https://wa.me/2348061632276?text=Hello%20there!%20I%20want%20to%20discuss%20with%20you."
-                    target="_blank"
+                    href='https://wa.me/2348061632276?text=Hello%20there!%20I%20want%20to%20discuss%20with%20you.'
+                    target='_blank'
                   >
-                    <i class="fa-brands fa-whatsapp"></i>
+                    <i className='fa-brands fa-whatsapp'></i>
                   </a>
                 </li>
               </ul>
-              <p className="socialPara">
-                We bring the years, global experience, and stamina to guide our
-                clients through new and often disruptive realities.
+              <p className='socialPara'>
+                We bring the years, global experience, and stamina to guide our clients through new
+                and often disruptive realities.
               </p>
             </div>
           </div>
         </div>
       </div>
-      <div className="lowerFooter">
-        More Deco – Where Dreams Meet Décor © {new Date().getFullYear()} All
-        Rights Reserved. Privacy Policy | Terms of Service
+      <div className='lowerFooter'>
+        More Deco – Where Dreams Meet Décor © {new Date().getFullYear()} All Rights Reserved.
+        Privacy Policy | Terms of Service
       </div>
     </div>
-  );
+  )
 }
