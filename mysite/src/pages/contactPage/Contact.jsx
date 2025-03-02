c9b4d89ecb25f4dc01622a71fbb7b8ae123af90b
Chibundu <ndubest56@gmail.com>
2025-03-02 13:42:12 +0100

    more on call

--- mysite/src/pages/contactPage/Contact.jsx
+++ mysite/src/pages/contactPage/Contact.jsx
@@ -1,39 +1,36 @@
-import React, { useState } from "react";
-import axios from "axios";
-import emailjs from "@emailjs/browser";
-import "./contact.css";
-import ScrollToTop from "../../components/ScrollToTop";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react'
+import axios from 'axios'
+import emailjs from '@emailjs/browser'
+import './contact.css'
+import ScrollToTop from '../../components/ScrollToTop'
+import { useNavigate } from 'react-router-dom'
 
 export default function Contact() {
-  const navigate = useNavigate();
-  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
+  const [loading, setLoading] = useState(false)
   const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    message: "",
-  });
-  const [status, setStatus] = useState("");
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+  })
+  const [status, setStatus] = useState('')
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
 
   const handleSubmit = async (e) => {
-    console.log(formData);
-    e.preventDefault();
-    setLoading(true);
+    console.log(formData)
+    e.preventDefault()
+    setLoading(true)
     try {
-      const response = await axios.post(
-        "https://moredeco.com.ng/contact.php",
-        formData
-      );
-      console.log(response);
-      setLoading(false);
-      setStatus("Message sent successfully!");
+      const response = await axios.post('https://moredeco.com.ng/contact.php', formData)
+      console.log(response)
+      setLoading(false)
+      setStatus('Message sent successfully!')
     } catch (error) {
-      setStatus("Failed to send message. Try again later.");
+      setStatus('Failed to send message. Try again later.')
     }
 
     //   emailjs
@@ -49,99 +46,95 @@ export default function Contact() {
     //         console.log("FAILED...", err);
     //       }
     //     );
-  };
+  }
 
   return (
-    <div className="contactUs">
+    <div className='contactUs'>
       <ScrollToTop />
-      <div className="contactFront">
-        <div className="contactOverlay"></div>
+      <div className='contactFront'>
+        <div className='contactOverlay'></div>
 
-        <div className="contactMain">
+        <div className='contactMain'>
           <div onClick={() => navigate(-1)}>
-            <i class="fa-solid fa-arrow-left fa-beat-fade iconStyle"></i>
+            <i class='fa-solid fa-arrow-left fa-beat-fade iconStyle'></i>
           </div>
-          <h2 className="contactHead">Contact Us Today</h2>
+          <h2 className='contactHead'>Contact Us Today</h2>
           <p>
-            At More-Links Deco, we believe every event deserves a touch of
-            magic. Whether you're planning a wedding, corporate event, birthday
-            party, or any special occasion, we're here to bring your vision to
-            life.
-            <hr /> Feel free to reach out using any of the methods below—we're
-            excited to hear from you!
+            At More-Links Deco, we believe every event deserves a touch of magic. Whether you're
+            planning a wedding, corporate event, birthday party, or any special occasion, we're here
+            to bring your vision to life.
+            <hr /> Feel free to reach out using any of the methods below—we're excited to hear from
+            you!
           </p>
         </div>
       </div>
 
-      <div className="contactSection">
-        <div className="contactDetails">
+      <div className='contactSection'>
+        <div className='contactDetails'>
           <h3>Get in Touch</h3>
-          <div className="fullDetails">
-            <p>📞 Phone: +2348061632276</p>
+          <div className='fullDetails'>
+            <p>📞 Phone: 08070958827</p>
+            <p>💬 Phone: 08061632276 </p>
             <p>📧 Email: services@moredeco.com</p>
             <p>📍 Location: 30 Adelabor Street Uwani, Enugu, Nigeria</p>
           </div>
         </div>
-        <div className="form">
+        <div className='form'>
           <form onSubmit={handleSubmit}>
             <h3>Tell us what is on your mind</h3>
             <div>
-              <label htmlFor="name">Name:</label>
+              <label htmlFor='name'>Name:</label>
               <input
                 required
-                type="text"
-                id="name"
-                name="name"
+                type='text'
+                id='name'
+                name='name'
                 value={formData.name}
                 onChange={handleChange}
-                placeholder="Your fullname"
+                placeholder='Your fullname'
               />
             </div>
             <div>
-              <label htmlFor="email">Email:</label>
+              <label htmlFor='email'>Email:</label>
               <input
                 required
-                type="email"
-                name="email"
+                type='email'
+                name='email'
                 value={formData.email}
-                id="email"
+                id='email'
                 onChange={handleChange}
-                placeholder="Active email"
+                placeholder='Active email'
               />
             </div>
             <div>
-              <label htmlFor="phone">Phone Number: </label>
+              <label htmlFor='phone'>Phone Number: </label>
               <input
                 required
-                type="text"
-                name="phone"
+                type='text'
+                name='phone'
                 value={formData.phone}
-                id="phone"
+                id='phone'
                 onChange={handleChange}
-                placeholder="Active phone number"
+                placeholder='Active phone number'
               />
             </div>
             <div>
-              <label htmlFor="message">Message for us </label>
+              <label htmlFor='message'>Message for us </label>
               <textarea
                 required
-                name="message"
-                id="message"
+                name='message'
+                id='message'
                 value={formData.message}
                 onChange={handleChange}
               ></textarea>
             </div>
-            <button className="btnSend">
-              {loading ? (
-                <i class="fa-solid fa-spinner fa-spin-pulse"></i>
-              ) : (
-                "Submit"
-              )}
+            <button className='btnSend'>
+              {loading ? <i class='fa-solid fa-spinner fa-spin-pulse'></i> : 'Submit'}
             </button>
             {status && <p>{status}</p>}
           </form>
         </div>
       </div>
     </div>
-  );
+  )
 }
